6655c95a65bd0280aaf6635deb50a10bc4e10039
Joel-S02 <joel.rajeshsingh@gmail.com>
2024-10-27 02:59:16 +0530

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(5 + 5) // we are using nodejs, not browser
+
+console.log(5 + 5) // code readability should be high
+
+console.log("Joel")
+
+
+let name = "Joel"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
