ad7ab5dde8d396642ace9cb8a50f743431eae0f9
Joel-S02 <joel.rajeshsingh@gmail.com>
2024-10-27 02:30:43 +0530

    Learnt Variables Const, Let, and Var.

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const userId = 12345;
+let userName = "Joel Singh";
+var userState = "Maharashtra";
+userCity = "Mumbai";
+
+//userName = "Raj Masih"// Let variables are used according to scope, like functions and loops use them and the rest also use them.
+//userId = 444 // Once the cost variable is defined, its value cannot be changed after that.
+console.table([userId, userName, userState, userCity]);
