079e6cc0eed28ca1dac99037a6fdb0bf40fd6758
Joel-S02 <joel.rajeshsingh@gmail.com>
2024-10-27 13:53:49 +0530

    Conversion DataType Operations

--- 01_basics/conversionDatatype.js
+++ 01_basics/conversionDatatype.js
@@ -1,15 +1,31 @@
 let isNumber = 50
 let numberToString = String(isNumber)
-console.log(isNumber) //50 in number
-console.log(numberToString) //50 in String
-console.log(typeof(numberToString)) // String
+// console.log(isNumber) //50 in number
+// console.log(numberToString) //50 in String
+// console.log(typeof(numberToString)) // String
 
-let numberToBoolean = Boolean(isNumber)
-console.log(numberToBoolean); // true with any number expact 0
-console.log(typeof(numberToBoolean)); // Boolean
+// let numberToBoolean = Boolean(isNumber)
+// console.log(numberToBoolean); // true with any number expact 0
+// console.log(typeof(numberToBoolean)); // Boolean
 
-console.log(typeof(isNumber)); // if isNumber is null then its type is object
-console.log(isNumber); // null
+// console.log(typeof(isNumber)); // if isNumber is null then its type is object
+// console.log(isNumber); // null
 
-console.log(typeof(isNumber)); // if isNumber is undefined then its type is also undefined
-console.log(isNumber); // undefined
+// console.log(typeof(isNumber)); // if isNumber is undefined then its type is also undefined
+// console.log(isNumber); // undefined
+
+/************** OPERATIONS **************/
+
+let str1 = "Joel"
+let str2 = "Singh"
+let str3 = str1 + " " + str2;
+// console.log(str3);
+
+// console.log(1 + "4");
+// console.log(5 + "5")
+// console.log("5" + 3 + 8)
+// console.log(3 + 8 + "5")
+// console.log(+true)
+// console.log(true)
+// console.log(+false)
+// console.log(false)
