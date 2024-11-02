f57c84277774014754e7fbda674a58bfa1d6e067
Joel-S02 <joel.rajeshsingh@gmail.com>
2024-11-02 13:10:33 +0530

    Numbers And Maths  Method

---
+++ 01_basics/numbersAndMaths.js
@@ -0,0 +1,21 @@
+const numbers = 50
+// console.log(numbers);
+
+const numbers2 = new Number(1065335460.342)
+// console.log(typeof numbers2)
+let score = numbers2;
+// // console.log(typeof score)
+// console.log(numbers2.toString())
+// console.log(numbers2.toFixed(5))
+// console.log(numbers2.toPrecision(8))
+// console.log(numbers2.toLocaleString('en-IN'))
+
+/***************************Maths***************************/
+// console.log(Math)
+// console.log(Math.abs(-77))//Returns the absolute value of a number (the value without regard to whether it is positive or negative). For example, the absolute value of -5 is the same as the absolute value of 5.
+// console.log(Math.ceil(7.1))//Returns the smallest integer greater than or equal to its numeric argument.
+// console.log(Math.floor(7.1))// Returns the greatest integer less than or equal to its numeric argument.
+// console.log(Math.round(75.80))// Returns a supplied numeric expression rounded to the nearest integer.
+// console.log(Math.random())//Returns a pseudorandom number between 0 and 1.
+// console.log(Math.min(2,23,22, 54, 34, 75, 94))// Returns the smaller of a set of supplied numeric expressions.
+// console.log(Math.max(34, 23, 75, 4, 87, 35, 65))// Returns the larger of a set of supplied numeric expressions.
