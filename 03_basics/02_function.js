c10e021366c4564453c67abe9d2507c116fa298c
Joel-S02 <joel.rajeshsingh@gmail.com>
2024-12-15 01:05:40 +0530

    Learnt how can i access Object and Array via function Parameter.

---
+++ 03_basics/02_function.js
@@ -0,0 +1,24 @@
+function calculateShoppingCart(num1, ...num){
+    return num;
+}
+
+// console.log(calculateShoppingCart(100, 200, 300))
+
+const person = {
+    name: "Joel",
+    age: 19,
+    gender: "Male"
+}
+
+function handleObject(useObject){
+    return `Person name is ${useObject.name} and ${useObject.age} years old and he is ${useObject.gender}`;
+}
+// console.log(handleObject(person));
+
+const myArr = ["Joel", "Manthan", "Mudit", "Rohit", "Abhishek (Bhaiya)"];
+
+function handleArray(useArray){
+    return useArray;
+}
+
+console.log(handleArray(myArr));
