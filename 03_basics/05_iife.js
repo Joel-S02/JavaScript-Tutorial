37dd416578e384ff6faaa7c45e5557fa2d92129b
Joel-S02 <joel.rajeshsingh@gmail.com>
2024-12-17 00:43:50 +0530

    IIFE (Immediately Invoked Functions Expressions)

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,8 @@
+// Immediately Invoked Functions Expressions
+(function helloWorld(lang){
+    console.log("Hello, ", lang);
+})("Javascript");
+
+(() => {
+    console.log("Hello, React JS")
+})();
